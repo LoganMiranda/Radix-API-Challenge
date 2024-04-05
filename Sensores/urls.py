011c9d5512174ce48874369f007f1632c1b57b7d
Logan Miranda <loganmiranda@id.uff.br>
2024-04-05 17:07:30 -0300

    definiçao das urls da api e criaçao da primeria api, que trata o JSON

---
+++ Sensores/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path, include
+from . import views
+
+app_name = 'Sensores'
+
+urlpatterns = [
+    path('api/Leitura_Json', views.dados_json, name='dados_json'),
+    #path('api/Leitura_CSV', views.dados_csv, name='dados_csv'),
+]
--- Sensores/views.py
+++ Sensores/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
+from .models import Sensor, Leitura
+import csv, json
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
+
+@csrf_exempt
+def dados_json(request):
+
+    if request.method == 'POST':
+        dados_sensor = json.loads(request.body)
+        equipmentId = dados_sensor["equipmentId"]
+        name = 'testex'+ equipmentId
+        
+        try:
+            if Sensor.objects.get(id_sensor = equipmentId):
+                mensagem = "Sensor ja existe"
+                return JsonResponse({"mensagem":mensagem})
+        
+        except:
+
+            #instancia novo objeto da classe Sensor
+            sensor = Sensor(id_sensor = equipmentId, nome = name )
+            #salva no BD
+            sensor.save()
+
+            mensagem = 'sensor nao existia, mas foi criado e inserido no BD com sucesso'
+            return JsonResponse({"mensagem":mensagem})
+        
